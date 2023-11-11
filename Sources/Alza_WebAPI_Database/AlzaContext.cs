2fa602bee23b9b9ab0e911887fc5fed8259efd4b
kubo10 <jakubvejcik@gmail.com>
2023-11-11 19:32:31 +0100

    #4 Add EF Core,  DbContext and init migration

---
+++ Sources/Alza_WebAPI_Database/AlzaContext.cs
@@ -0,0 +1,23 @@
+﻿using Alza_WebAPI_Database.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Alza_WebAPI_Database
+{
+    public class AlzaContext : DbContext
+    {
+        private readonly IConfiguration Configuration = default!;
+
+        public AlzaContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Sources/Alza_WebAPI_Database/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alza_WebAPI_Database.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; } = default!;
+
+        [Required]
+        [MaxLength(400)]
+        public string ImgUri { get; set; } = default!;
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; } 
+
+        [MaxLength(4000)]
+        public string Description { get; set; } = default!;    
+    }
+}
